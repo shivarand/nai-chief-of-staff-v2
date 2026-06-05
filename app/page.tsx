8cda4f30365395cbd7d13751b2dc5f3a2d84345c
shivarand <9.jearasinkul@gmail.com>
2026-06-05 19:26:08 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Nai Chief of Staff Bot</h1>
+      <p>LINE Bot is running.</p>
+    </main>
+  );
+}
